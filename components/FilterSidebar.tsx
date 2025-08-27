6804bfc6bc06a90ceb0228913e52c6bd4d5c90f4
Azeem <bljazeem@gmail.com>
2025-08-27 19:44:27 +0100

    fix: update location input placeholder for clarity

--- components/FilterSidebar.tsx
+++ components/FilterSidebar.tsx
@@ -120,7 +120,7 @@ export default function FilterSidebar({
               <MapPin className="absolute left-3 top-1/2 -translate-y-1/2 h-5 w-5 text-gray-400" />
               <Input
                 id="location"
-                placeholder="New York, USA"
+                placeholder="Type in a location..."
                 className="pl-10 focus-visible:ring-red-500"
                 value={location}
                 onChange={e => setLocation(e.target.value)}
